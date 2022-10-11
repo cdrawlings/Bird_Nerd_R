ba2a290048559c7d17196ec5dba588a58bd65f03
chrisrawlings <c.d.rawlings@gmail.com>
2022-10-11 11:20:54 -0400

    User LOgin Registratin set up

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-console.log('Hello')
+const express = require('express')
+const Process = require("process");
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 8000
+
+//Connect to database
+connectDB()
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
