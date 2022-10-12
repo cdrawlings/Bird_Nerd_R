e6d0b922e4e1a576b50050a3f7357467157645d0
chrisrawlings <c.d.rawlings@gmail.com>
2022-10-12 17:34:09 -0400

    Authorization working

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {useSelector, useDispatch} from "react-redux";
+import {useNavigate} from "react-router-dom";
 import {toast} from 'react-toastify'
 import {FaSignInAlt} from 'react-icons/fa'
-import {login} from '../features/auth/authSlice'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/spinner'
 
 
 function Login() {
@@ -13,10 +15,24 @@ function Login() {
     })
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const {user, isLoading, isSuccess, isError, message } = useSelector(state => state.auth)
 
     const {email, password} = formData
+
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        }
+
+        // Redirect when logged in
+        if(isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset())
+    }, [isSuccess, isError, message, user, navigate, dispatch])
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -32,6 +48,10 @@ function Login() {
                 password
             }
         dispatch(login(userData))
+    }
+
+    if (isLoading){
+        return <Spinner />
     }
 
     return (
--- frontend/src/pages/registration.js
+++ frontend/src/pages/registration.js
@@ -5,7 +5,7 @@ import {useSelector, useDispatch} from "react-redux";
 import {toast} from 'react-toastify'
 import {FaUser} from 'react-icons/fa'
 import {register, reset} from '../features/auth/authSlice'
-
+import Spinner from '../components/spinner'
 
 
 function Registration() {
@@ -58,6 +58,10 @@ function Registration() {
             }
           dispatch(register(userData))
         }
+    }
+
+    if (isLoading){
+        return <Spinner />
     }
 
     return (
